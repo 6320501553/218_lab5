2cb96adb16562d9170c1554a71bc48b5ea584c6f
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-03-11 17:02:00 +0700

    add

--- star_5_2.c
+++ star_5_2.c
@@ -7,14 +7,14 @@ int main()
         m=n;
     else
         m=n-1;
-    for(i=0;i<n;i++)
+    for(i=0; i<n/2; i++)
     {
-        for(j=0;j<m;j++)
+        for(j=0; j<m; j++)
         {
             li=m/2;
-            if(i==0||i==(n-1)) //บรรทัดที่แรกและสุดท้าย
+            if(i==0) //บรรทัดที่แรกและสุดท้าย
             {
-                if(j==li++)
+                if(j==li)
                     printf("*");
                 else
                     printf("_");
@@ -26,23 +26,38 @@ int main()
                 else
                     printf("_");
             }
-            else if(i<=li) //บรรทัดที่สองและรองสุดท้าย
+            else if(i<=li)
             {
                 if(j==(li-k)||j==(li+k))
                     printf("*");
                 else
                     printf("_");
             }
-            /*else
+            else
             {
-                if(i<(li+1)||i>li)
-                {
-                    if(j==(n)||j==(n-2))
-                        printf("*");
-                    else
-                        printf("_");
-                }
-            }*/
+                if(j==(li+k)||j==(li-k))
+                    printf("*");
+                else
+                    printf("_");
+            }
+        }
+        printf("\n");
+        k++;
+    }
+    k=0;
+    if(n%2==0)
+        n=n/2;
+    else
+        n=(n/2)+1;
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<m; j++)
+        {
+
+            if(j==k||j==(m-(k+1)))
+                printf("*");
+            else
+                printf("_");
         }
         printf("\n");
         k++;
