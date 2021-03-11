26064fa6022ecb0c5f45525e3597efbeb36af074
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-03-11 15:28:46 +0700

    add

--- star_5_2.c
+++ star_5_2.c
@@ -1,13 +1,38 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,m,li;
     scanf("%d",&n);
+    if(n%2==1)
+        m=n;
+    else
+        m=n-1;
     for(i=0;i<n;i++)
     {
-        for(j=0;j<n-1;j++)
+        for(j=0;j<m;j++)
         {
-            printf("*");
+            li=m/2;
+            if(i==0||i==(n-1))
+            {
+                if(j==li++)
+                    printf("*");
+                else
+                    printf("-");
+            }
+            else if(n>4&&i==1||i==(n-2))
+            {
+                if(j%2!=0)
+                    printf("*");
+                else
+                    printf("-");
+            }
+            else if(i==li++||i==li)
+            {
+                if(j==0||j==(m-1))
+                    printf("*");
+                else
+                    printf("-");
+            }
         }
         printf("\n");
     }
