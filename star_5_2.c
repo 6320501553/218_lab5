a6842acfe9aa9d6247d4bae2f25ece1c05afc68c
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-03-11 15:52:26 +0700

    edit

--- star_5_2.c
+++ star_5_2.c
@@ -12,27 +12,28 @@ int main()
         for(j=0;j<m;j++)
         {
             li=m/2;
-            if(i==0||i==(n-1))
+            if(i==0||i==(n-1)) //บรรทัดที่แรกและสุดท้าย
             {
                 if(j==li++)
                     printf("*");
                 else
                     printf("_");
             }
-            else if(n>4&&(i==1||i==(n-2)))
+            else if(i==1||i==(n-2)) //บรรทัดที่สองและรองสุดท้าย
             {
-                if(j%2!=0)
+                if(j==(li-1)||j==(li+1))
                     printf("*");
                 else
                     printf("_");
             }
-            else if(i==li++||i==li)
+            else if((i==(li+1)&&(n%2==0))||i==li) //บรรทัดตรงกลาง
             {
                 if(j==0||j==(m-1))
                     printf("*");
                 else
                     printf("_");
             }
+
         }
         printf("\n");
     }
